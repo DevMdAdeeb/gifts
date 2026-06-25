90536d06a78e001cdf70eb8d5eb616e9adaed3d6
Mohammed adeeb <dev.md.adeeb@gmail.com>
2026-06-26 00:31:00 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+API_ID = 23185058  # احصل عليه من my.telegram.org
+API_HASH = 'ae6e1ebaac4338c4d2a04a245f47cbc8'  # احصل عليه من my.telegram.org
+BOT_TOKEN = '8468078030:AAEMoKrfi1izbDRA1FAiMMC-Q_kumwi1qwM'  # احصل عليه من BotFather
+ADMIN_ID = 873649941  # معرف المدير
+PHONE_NUMBER = '+967738871334' # رقم هاتف حساب المستخدم للمراقبة (مع رمز الدولة)
+
+ALERT_PERCENTAGE = 20  # النسبة المئوية الافتراضية للتنبيه
+MONITOR_INTERVAL = 5  # فترة المراقبة الافتراضية بالدقائق
+
+# توقيت اليمن (UTC+3)
+TIMEZONE = 'Asia/Aden'
+
+USER_SESSION_NAME = 'nft_user_session'
+BOT_SESSION_NAME = 'nft_bot_session'
+
